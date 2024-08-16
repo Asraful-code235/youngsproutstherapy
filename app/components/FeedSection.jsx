eab63d565ee71073cf6833cee7736b31510f062f
Asraful-code235 <asrafulislamcodes@gmail.com>
2024-08-16 09:45:37 +0600

    updated latest blog sections

--- app/components/FeedSection.jsx
+++ app/components/FeedSection.jsx
@@ -3,8 +3,10 @@ import BlogCard from "../blog/components/BlogCard";
 
 export default function FeedSection({ data }) {
   return (
-    <section className="mt-8 px-8 lg:px-[94px] py-12">
-      <h2 className="text-2xl font-semibold mb-4">Latest Blog Posts</h2>
+    <section className="mt-8 px-8 lg:px-[94px] py-12 pb-16 lg:pb-20">
+      <h2 className="text-4xl font-bold text-center text-[#cca4a4] md:text-5xl gap-14 lg:mb-16">
+        Latest Blog Posts
+      </h2>
       <div className="relative">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 lg:gap-8">
           {data.map((post) => (
